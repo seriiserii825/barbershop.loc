ac31f18e0d936498bff8564607cd21a617e9c96a
serii <seriiburduja@gmail.com>
2019-02-26 19:14:49 +0200

    check svg

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,11 @@ wait = require('gulp-wait'),
 pug = require('gulp-pug'),
 //js
 babel = require("gulp-babel"),
+//sprite
+  svgSprite = require('gulp-svg-sprite'),
+	svgmin = require('gulp-svgmin'),
+	cheerio = require('gulp-cheerio'),
+	replace = require('gulp-replace'),
 //settings
 notify = require("gulp-notify"),
 rigger = require("gulp-rigger"),
@@ -19,7 +24,41 @@ browserSync = require('browser-sync').create(),
 rimraf = require("rimraf"),
 gp = require('gulp-load-plugins')();
 
-
+gulp.task('svgSprite', function () {
+	return gulp.src('src/assets/i/icons/svg/inline/*.svg')
+	// minify svg
+		.pipe(gp.svgmin({
+			js2svg: {
+				pretty: true
+			}
+		}))
+		// remove all fill, style and stroke declarations in out shapes
+		.pipe(gp.cheerio({
+			run: function ($) {
+				$('[fill]').removeAttr('fill');
+				$('[stroke]').removeAttr('stroke');
+				$('[style]').removeAttr('style');
+			},
+			parserOptions: {xmlMode: true}
+		}))
+		// cheerio plugin create unnecessary string '&gt;', so replace it.
+		.pipe(gp.replace('&gt;', '>'))
+		// build svg sprite
+		.pipe(gp.svgSprite({
+			mode: {
+				symbol: {
+					sprite: "../sprite.svg",
+					render: {
+						scss: {
+							dest:'../../../sass/_sprite.scss',
+							template: "src/assets/sass/templates/_sprite_template.scss"
+						}
+					}
+				}
+			}
+		}))
+		.pipe(gulp.dest('/build/assets/i/sprite/'));
+});
 
 gulp.task('pug', function(){
   return gulp.src('src/pug/pages/*.pug')
